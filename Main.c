b71cb3393d77dfdefeab14a2f242dea43357bedd
zain-erto <dingzhixian2014@163.com>
2026-07-01 20:58:52 +0800

    Add loop structure for counting down from 5

---
+++ Main.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+  unsigned long long a;
+  unsigned long long m=2413;
+  if(a==m)
+  {
+    while(1)
+    {
+    }
+  }
+  else
+  {
+    int i;
+    for(i=5;i>=1;i++)
+    {
+        //printf();
+    }
+  }
+  return 0;
+}
